9611c141ad8c8870c72086b997d253d993afb6ef
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-03-30 18:45:33 -0700

    creates route and skeleton action for home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :pieces
+  get :home, to: "application#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
