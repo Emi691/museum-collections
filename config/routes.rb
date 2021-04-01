b5f40abbf656b1879cfb2d58773f9ddd20431d34
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-03-31 21:33:56 -0700

    creates session controller and rewrites logout route to sessions insead of users

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   post '/signup', to: "users#create"
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
-  get "/logout", to: "users#logout"
+  get "/logout", to: "sessions#logout"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
