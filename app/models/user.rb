b2b5e88e2639108cc74460554f8058e90378d668
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-03-31 18:57:58 -0700

    adds validations and has secure password for users, also adds bcrypt and pry gems to gemfile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
     has_many :pieces
     has_many :museums, through: :pieces
+
+    has_secure_password
+    
+    validates_presence_of :username, :email
+    validates_uniqueness_of :username, :email
 end
