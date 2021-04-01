79e07c9574e1eb5f0c16676118caae81582d734d
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-03-31 19:02:27 -0700

    adds validations for treatment and conditon models

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -1,4 +1,6 @@
 class Condition < ApplicationRecord
     belongs_to :treatment
     belongs_to :piece
+
+    validates_presence_of :description, :date_time
 end
--- app/models/treatment.rb
+++ app/models/treatment.rb
@@ -1,4 +1,6 @@
 class Treatment < ApplicationRecord
     has_many :conditions
     has_many :pieces, through: :conditions
+
+    validates_presence_of :description, :date_time
 end
