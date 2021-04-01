5fa44501bb46b1c73331b42645b38e5887ddbc00
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-03-31 21:31:33 -0700

    creates routes for login, signup, and logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   resources :pieces
   get "/", to: "application#home"
 
-  get "/signup", to: "users#signup"
-  get "/login", to: "users#login"
+  get "/signup", to: "users#new"
+  post '/signup', to: "users#create"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
   get "/logout", to: "users#logout"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
