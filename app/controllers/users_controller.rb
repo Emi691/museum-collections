136b466d39ddf45ae16f6e4502c5e9d920120e0a
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-03-31 22:01:17 -0700

    fixes bugs with user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,9 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
+        #binding.pry
         if @user.save
-            session[:user_id] = @user.user_id
+            session[:user_id] = @user.id
             redirect_to pieces_path
         else
             render :new
