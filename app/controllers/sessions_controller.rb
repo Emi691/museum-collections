dab81c8941922b12973a010f41572646033f3615
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-04-01 17:52:46 -0700

    works out bugs with omniauth to allow login via google and starts to fill out create method in sessions to accomodate this

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,20 @@ class SessionsController < ApplicationController
     end
 
     def create
-        @user = User.find_by(username: session_params[:username])
-        if @user && @user.authenticate(session_params[:password])
+        binding.pry
+        if google_auth
+            @user = user.find_or_create_by(username: google_auth[:name], email: google_auth[:email])
             session[:user_id] = @user.id
-            redirect_to pieces_path
+                redirect_to pieces_path
         else
-            @user = User.new(session_params)
-            render :new
+            @user = User.find_by(username: session_params[:username])
+            if @user && @user.authenticate(session_params[:password])
+                session[:user_id] = @user.id
+                redirect_to pieces_path
+            else
+                @user = User.new(session_params)
+                render :new
+            end
         end
     end
 
@@ -20,6 +27,10 @@ class SessionsController < ApplicationController
 
     private
     
+    def google_auth
+        request.env['omniauth.auth']
+    end
+
     def session_params
         params.require(:user).permit(:username, :password)
     end
