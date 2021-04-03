5d40372c5a997bf9d573b68b671d64295d0b2b7d
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-04-02 21:16:52 -0700

    creates helper methods to find current user, check if logged_in and redirect if not logged in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
     def home
     end
+
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+
+    def logged_in?
+        !!session[:user_id]
+    end
+
+    def redirect_if_not_logged_in
+        if !logged_in?
+            redirect_to '/'
+        end
+    end
 end
