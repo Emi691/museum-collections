2914449458eff1ff4d3ba044a187c366c618e272
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-04-04 18:35:33 -0700

    adds parse_time instance method to treatment and condition models and implements if where needed.

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -2,5 +2,9 @@ class Condition < ApplicationRecord
     belongs_to :treatment, optional: true
     belongs_to :piece, optional: true
 
-    #validates_presence_of :description, :date_time
+    validates_presence_of :description, :date_time
+    
+    def parse_time
+        self.date_time.strftime("%d %B, %Y")
+    end
 end
--- app/models/treatment.rb
+++ app/models/treatment.rb
@@ -5,4 +5,8 @@ class Treatment < ApplicationRecord
     accepts_nested_attributes_for :conditions 
 
     validates_presence_of :description, :date_time
+
+    def parse_time
+        self.date_time.strftime("%d %B, %Y")
+    end
 end
